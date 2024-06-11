fe5611977f9a32145a7c8cccfa4be3059b941a4d
cccaelum <ccc.trad@gmail.com>
2024-06-11 11:45:08 +0200

    ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
  // Aquí tu código
+ const agregar = document.getElementById('agregar');
+ const lista = document.getElementById('lista');
+
+ agregar.addEventListener('click', function() {
+   const elementoNuevo = prompt('Agrega un nuevo elemento:');
+
+   if (elementoNuevo) {
+    const li = document.createElement('li'); 
+    li.textContent = elementoNuevo;
+    lista.appendChild(li);}
+});
+
+ 
