ebefcf890f4ac65132e00deb459c0b60d5bdf0ec
Celia Cebaquebas Calero <ccc.trad@gmail.com>
2024-06-12 10:30:06 +0200

    comentarios en js tras review

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
  const agregar = document.getElementById('agregar');
  const lista = document.getElementById('lista');
 
+// Es mejor meter la funcion fuera del addeventlistener, por si queremos reutilizarla
  agregar.addEventListener('click', function() {
    const elementoNuevo = prompt('Agrega un nuevo elemento:');
 
@@ -11,4 +12,4 @@
     lista.appendChild(li);}
 });
 
- 
+// Al final del if podemos poner un else con un alert por si el usuario no introduce un dato 
